35cdc6b26f0bab51d6003d9eff49fabc7e6633ef
Jacky Liang <asheriffy@gmail.com>
2022-03-16 13:31:18 -0400

    updated rote.js

---
+++ rote.js
@@ -0,0 +1,15 @@
+const greet = () => {
+    console.log('Hello World')
+}
+greet()
+
+const square = (num) => {
+    return num * num
+}
+square(25)
+
+const readByName = (str) => {
+    console.log('Hello', str)
+}
+
+greetByName('Jacky')
