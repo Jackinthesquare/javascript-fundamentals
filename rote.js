4eed0cae2fd3a93c637b3f9531634a0c72e22a36
Jacky Liang <asheriffy@gmail.com>
2022-03-17 11:06:45 -0400

    added apartments

--- rote.js
+++ rote.js
@@ -1,15 +1,17 @@
 const greet = () => {
-    console.log('Hello World')
+    console.log('Hello World!')
 }
+
 greet()
 
 const square = (num) => {
     return num * num
 }
-square(25)
 
-const readByName = (str) => {
-    console.log('Hello', str)
+square(7)
+
+const greetByName = (str) => {
+    console.log('Hello,', str)
 }
 
 greetByName('Jacky')
